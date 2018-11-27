de13645cdd261de884d4ec3d5df7e0dc0fd64f5f
william <williamaddress>
2018-11-27 16:31:28 +0800

    更新产品投诉子表-问题产品台帐

---
+++ src/weaver/interfaces/workflow/action/UpdateWtcptzHfzc.java
@@ -0,0 +1,67 @@
+package weaver.interfaces.workflow.action;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+import weaver.conn.RecordSet;
+import weaver.general.Util;
+import weaver.soa.workflow.request.RequestInfo;
+
+import com.weaver.ningb.soa.workflow.action.support.ActionInfo;
+import com.weaver.ningb.soa.workflow.action.support.ActionUtils;
+/**
+ * 问题产品恢复正常申请单结束节点更新产品投诉子表-问题产品台帐台帐<br>
+ * 
+ * @author ycj
+ *
+ */
+public class UpdateWtcptzHfzc implements Action
+{
+  private Log logger = LogFactory.getLog(UpdateWtcptzHfzc.class);
+  
+  @Override
+  public String execute(RequestInfo request)
+  {
+	String requestid = request.getRequestid();
+    
+	RecordSet rs = new RecordSet();
+    
+    String sql = "";
+    String RQ = "";
+    try
+    {
+    	ActionInfo info = ActionUtils.getActionInfo(request);
+        
+    	// 获取明细表1信息
+	    List<Map<String, String>> detailAList = info.getDetailMap("1");
+	    if (detailAList != null && detailAList.size() > 0) {
+	        RQ = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
+			for (int i = 0; i < detailAList.size(); i++) {
+				Map<String, String> detailAMap = detailAList.get(i);
+				String hhDetailA = Util.null2String(detailAMap.get("HH"));		//货号
+				String hfyyDetailA = Util.null2String(detailAMap.get("HFYY"));	//恢复原因
+				sql = "update uf_CPTSTZ set CPZLZT = '1',HFYY = '" + hfyyDetailA + 
+						"',RQ = '" + RQ + "',HFLC = '" + requestid + 
+						"' where id = '" + hhDetailA + "'";
+				rs.execute(sql);
+			}
+		}
+        //this.logger.error("sql：" + sql);
+        
+    }
+    catch (Exception e)
+    {
+      this.logger.error("Exception e", e);
+      request.getRequestManager().setMessageid("1111111113");
+      request.getRequestManager().setMessagecontent(e + ",请联系管理员.");
+      return Action.FAILURE_AND_CONTINUE;
+    }
+    return Action.SUCCESS;
+      
+  }
+}
